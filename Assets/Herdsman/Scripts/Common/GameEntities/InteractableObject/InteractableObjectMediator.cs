07f1ad977cd5585de96f85432858be57aad7a27c
dmytro.martynov <dmitriy.martynov@whimsygames.co>
2023-07-22 12:18:02 +0300

    Add base for interactable objects

---
+++ Assets/Herdsman/Scripts/Common/GameEntities/InteractableObject/InteractableObjectMediator.cs
@@ -0,0 +1,20 @@
+﻿using Common.GameEntities.Abstract;
+
+namespace Common.GameEntities.InteractableObject
+{
+    public class InteractableObjectMediator : GameEntityMediatorBase<InteractableObjectView>
+    {
+        //Class for base interactable object (loot, other scene objects for interact)
+        //Object is static (doesn't move)
+        
+        protected override void OnViewReady()
+        {
+            //Add custom components to view
+        }
+
+        protected override void OnDestroy()
+        {
+            //Remove custom components from view
+        }
+    }
+}
---
+++ Assets/Herdsman/Scripts/Common/GameEntities/InteractableObject/InteractableObjectView.cs
@@ -0,0 +1,9 @@
+﻿using Common.GameEntities.Abstract;
+
+namespace Common.GameEntities.InteractableObject
+{
+    public class InteractableObjectView : GameEntityViewBase
+    {
+        
+    }
+}
